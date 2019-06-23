b5be797836e0a01af1389b648c6b2574f8f0ffc8
Margot Konig <margot.konig@blackrock.com>
2019-06-23 03:10:27 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do
+  "Hello, World!"
+  end
+  
+  get '/name' do
+    "My name is Margot."
+  end 
+  
+  get '/hometown' do 
+    "My hometown is Dallas."
+  end 
+  
+  get '/favorite-song' do
+    "My favorite song is Mother's Daughter."
+  end 
+
 end
