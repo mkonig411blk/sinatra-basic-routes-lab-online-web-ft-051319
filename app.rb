5ae0843d56646e17833dd1c8476b98584d73fba2
Margot Konig <margot.konig@blackrock.com>
2019-06-23 03:06:38 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,7 +6,16 @@ class App < Sinatra::Base
   "Hello, World!"
   end
   
-  get '/name' do 
-    "My name is "
+  get '/name' do |name|
+    "My name is #{name}."
+  end 
+  
+  get '/hometown' do |hometown|
+    "My hometown is #{hometown}."
+  end 
+  
+  get '/favorite-song' do |song|
+    "My favorite song is #{song}."
+  end 
 
 end
